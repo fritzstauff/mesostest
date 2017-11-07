6be6676f915c3ea993d2ca182778ff90557b9264
Fritz <fritz@fritzs-mbp.hvvc.local>
2017-11-07 13:24:41 -0500

    Test real ip

--- checkin.py
+++ checkin.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 
 @app.route("/getip", methods=["GET"])
 def get_my_ip():
-  if request.headers.getlist("X-Forwarded-For"):
-     ip = request.headers.getlist("X-Forwarded-For")[0]
+  if request.headers.getlist("X-REAL-IP"):
+     ip = request.headers.getlist("X-REAL-IP")[0]
   else:
      ip = request.remote_addr
   
